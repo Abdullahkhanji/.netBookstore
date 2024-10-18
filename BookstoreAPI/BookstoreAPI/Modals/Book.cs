918c0f6ef6d97de0e05e45f9e09589848b700d0d
Abdullahkhanji <abdullah206khan@gmail.com>
2024-10-19 00:14:31 +0300

    Removed nested repository from index

---
+++ BookstoreAPI/BookstoreAPI/Modals/Book.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookstoreAPI.Modals
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Author { get; set; }
+
+        [Column(TypeName = "nvarchar(500)")]
+        public string Description { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Publisher { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string PublicationDate { get; set; }
+
+        [Column(TypeName = "int")]
+        public int PageCount { get; set; }
+
+        [Column(TypeName = "nvarchar(500)")]
+        public string Cover { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string PDFFile { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public int UID { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string UploudDate { get; set; }
+
+        [Column(TypeName = "int")]
+        public int DownloadCount { get; set; }
+
+        [Column(TypeName = "int")]
+        public int ViewCount { get; set; }
+    }
+}
---
+++ BookstoreAPI/BookstoreAPI/Modals/BookContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreAPI.Modals
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
